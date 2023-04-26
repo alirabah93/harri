5863c4bb9a87083f0aac753507e47cec76e468f7
alirabah93 <110853127+alirabah93@users.noreply.github.com>
2023-04-26 14:00:17 +0300

    create splash screen for 3 seconds

---
+++ JobFinder/app/src/main/java/com/example/jobfinder/SplashScreen.kt
@@ -0,0 +1,22 @@
+package com.example.jobfinder
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+//      the following handler display the splash screen at the app lunching for 3 seconds
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
