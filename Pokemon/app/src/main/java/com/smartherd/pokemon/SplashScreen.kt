39826e4abb711bea2d0f43eb67caf4f848eba91a
alirabah93 <110853127+alirabah93@users.noreply.github.com>
2023-05-13 12:45:03 +0300

    Pokemon splash screen is ready for review

---
+++ Pokemon/app/src/main/java/com/smartherd/pokemon/SplashScreen.kt
@@ -0,0 +1,21 @@
+package com.smartherd.pokemon
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        //      the following handler display the splash screen at the app lunching for 3 seconds
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
