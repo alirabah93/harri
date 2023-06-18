0d4aa21453d29a9e99ddaeecc6fd43b718a5b9c2
alirabah93 <110853127+alirabah93@users.noreply.github.com>
2023-06-18 13:47:49 +0300

    applying stat circle progress bar

--- Pokemon/app/src/main/java/com/smartherd/pokemon/detail/PokemonDetailViewHolder.kt
+++ Pokemon/app/src/main/java/com/smartherd/pokemon/detail/PokemonDetailViewHolder.kt
@@ -1,17 +1,21 @@
 package com.smartherd.pokemon.detail
 
-import android.view.View
-import android.widget.TextView
+
 import androidx.recyclerview.widget.RecyclerView
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.TextView
 import com.smartherd.pokemon.R
 import com.smartherd.pokemon.models.PokemonStat
 
 class PokemonDetailViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private val pokemonStatInt: TextView = itemView.findViewById(R.id.stat_int)
     private val pokemonStatName: TextView = itemView.findViewById(R.id.stat_name)
+    private val statProgress: ProgressBar = itemView.findViewById(R.id.stat_progress_bar)
 
     fun bindPokemonDetail(stat: PokemonStat) {
         pokemonStatInt.text = stat.base_stat.toString()
         pokemonStatName.text = stat.stat.name
+        statProgress.progress = stat.base_stat * 100 / 255
     }
 }
